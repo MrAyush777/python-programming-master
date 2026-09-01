fa964193f6644349aa6c62f0f24e29fb48a4ee5a
MrAyush777 <ayushfataniya2005@gmail.com>
2026-09-02 01:08:45 +0530

    Intro of Inheritance

--- chapter_6_ObjectOrientedProgramming/OOP_10_inheritance.py
+++ chapter_6_ObjectOrientedProgramming/OOP_10_inheritance.py
@@ -7,8 +7,20 @@ class Vehicle:
         self.mileAge = mileage
         
     def getDetails(self):
-        return f"This vehicle has {self.nWheels}, {self.nSeats} and provides mileage of {self.mileAge} km/litre."
+        return f"This vehicle has {self.nWheels} wheels, {self.nSeats} seats and provides mileage of {self.mileAge} km/litre."
     
     
 v1 = Vehicle(4,5,15)
 print(v1.getDetails())
+
+# Now let's see how inheritance works in python
+
+# below Vehicle class is inherited by Car class
+
+class Car(Vehicle):
+    pass
+# car class is called child/sub/derived class
+# vehhicle class is called parent/super/base class
+
+
+# c1 = Car() # it will give error because we have not passed the required arguments in the constructor of Vehicls class. So we need to pass the required arguments in the constructor of Car class as well. Because Car class is inheriting the constructor of Vehicle class.
